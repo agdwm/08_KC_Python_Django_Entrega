5336e186562dd1950904ce3952aeec295007a8c6
agdwm <almudenaguerraswm@gmail.com>
2018-01-13 21:57:36 +0100

    Registered models the in Django Admin Site

--- WORDPLEASE/src/blogs/models.py
+++ WORDPLEASE/src/blogs/models.py
@@ -1,22 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
+
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
 
+    # Returns the representation of an object as a string.
+    # It's very useful for working with the Django Admin Panel
     def __str__(self):
         return self.name
 
 
 class Blog(models.Model):
+
     blog_title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    # Si el usuario se elimina de la BBDD se eliminarán también los blogs de dicho usuario.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -24,8 +27,9 @@ class Blog(models.Model):
 
 
 class Post(models.Model):
+
     post_title = models.CharField(max_length=50)
-    headline = models.TextField(max_length=100)
+    headline = models.CharField(max_length=100)
     content = models.TextField()
     media = models.URLField(blank=True, null=True)
     release_date = models.DateTimeField()
@@ -33,8 +37,8 @@ class Post(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
 
-    created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # saves the date when the object is created
+    modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
 
     def __str__(self):
         return self.post_title
